33c7a9db340d6f8a86e82cb64e9037d73abcecbf
gilly002 <gilbertamenorhu@gmail.com>
2023-10-17 02:32:40 -0700

    exit_shell.c

--- exit_shell.c
+++ exit_shell.c
@@ -4,11 +4,12 @@
 #include "shell.h"
 
 /**
-* The function "exit_shell" is used to exit the shell with a specified status.
+* The function "exist_shell" is used to exit the shell with a specified status.
 *
-* @param status The status parameter is an integer value that represents the exit status of the shell.
-* It is typically used to indicate whether the shell terminated successfully or encountered an error.
-* A value of 0 usually indicates successful termination, while non-zero values indicate an error or
+* @param status represents the exit status of the shell.
+* Check whether the shell terminated successfully or encountered an error.
+* A value of 0 usually indicates successful termination
+* While non-zero values indicate an error or
 * abnormal termination.
 */
 void exit_shell(int status)
